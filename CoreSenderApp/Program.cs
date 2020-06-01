1f7eedb199259e90ed406a70ccaefcc0bf4de9e1
Andres Arias <andres.k09@gmail.com>
2020-06-01 12:25:30 +0200

    Test simple queue

---
+++ CoreSenderApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreSenderApp
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "<your_connection_string>";
+        const string QueueName = "<your_queue_name>";
+        static IQueueClient queueClient;
+
+        static async Task Main(string[] args)
+        {
+            const int numberOfMessages = 10;
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Send messages.
+            await SendMessagesAsync(numberOfMessages);
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+
+
+        }
+
+        private static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (int i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the queue.
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the queue.
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # azure-service-bus
+
+Microsoft Quickstart for Service Bus queues
+
+Create resources
+
+```
+# Create a resource group
+resourceGroupName="learn-rg-$RANDOM"
+
+az group create --name $resourceGroupName --location westeurope
+
+# Create a Service Bus messaging namespace with a unique name
+namespaceName=learn-namespace-$RANDOM
+az servicebus namespace create --resource-group $resourceGroupName --name $namespaceName --location westeurope
+
+# Create a Service Bus queue
+az servicebus queue create --resource-group $resourceGroupName --namespace-name $namespaceName --name BasicQueue
+
+# Get the connection string for the namespace
+connectionString=$(az servicebus namespace authorization-rule keys list --resource-group $resourceGroupName --namespace-name $namespaceName --name RootManageSharedAccessKey --query primaryConnectionString --output tsv)
+
+```
